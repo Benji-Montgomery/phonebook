f1ea71807ce6704515350a506e44df1bb507ca15
Benji-Montgomery <benjimon678@yahoo.com>
2022-11-27 07:46:00 -0800

    secondtrySunday

--- index.js
+++ index.js
@@ -2,28 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-let persons =[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons =[]
 
 //const requestLogger = (request, response, next) => {
 //  console.log('Method:', request.method)
